c1b6a1e00268e2546b9acbebe142f32e97cab20c
kikyo <cyborgz1999@gmail.com>
2023-11-09 11:49:25 +0800

    add test case for cte

--- regression-test/suites/nereids_rules_p0/cte/test_cte_filter_pushdown.groovy
+++ regression-test/suites/nereids_rules_p0/cte/test_cte_filter_pushdown.groovy
@@ -20,11 +20,30 @@ suite("test_cte_filter_pushdown") {
     sql "SET enable_fallback_to_original_planner=false"
     sql "set enable_runtime_filter_prune=false"
     // CTE filter pushing down with the same filter
+    sql """
+        CREATE TABLE IF NOT EXISTS `test` (
+            `k0` boolean null comment "",
+            `k1` tinyint(4) null comment "",
+            `k2` smallint(6) null comment "",
+            `k3` int(11) null comment "",
+            `k4` bigint(20) null comment "",
+            `k5` decimal(9, 3) null comment "",
+            `k6` char(5) null comment "",
+            `k10` date null comment "",
+            `k11` datetime null comment "",
+            `k7` varchar(20) null comment "",
+            `k8` double max null comment "",
+            `k9` float sum null comment "",
+            `k12` string replace_if_not_null null comment "",
+            `k13` largeint(40) replace null comment ""
+        ) engine=olap
+        DISTRIBUTED BY HASH(`k1`) BUCKETS 5 properties("replication_num" = "1")
+        """
     qt_cte_filter_pushdown_1 """
             explain shape plan
             with main AS (
                select k1, row_number() over (partition by k1) rn
-               from nereids_test_query_db.test
+               from test
            )
            select * from (
                select m1.* from main m1, main m2
@@ -36,7 +55,7 @@ suite("test_cte_filter_pushdown") {
             explain shape plan
             with main AS (
                select k1, row_number() over (partition by k2) rn
-               from nereids_test_query_db.test
+               from test
            )
            select * from (
                select m1.* from main m1, main m2
