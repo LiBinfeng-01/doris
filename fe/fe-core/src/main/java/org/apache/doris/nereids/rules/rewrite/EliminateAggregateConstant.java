db9f2f9eff97df96eeaad2558924ed92d790fa41
LiBinfeng <1204975323@qq.com>
2024-03-13 17:13:55 +0800

    [Feat](Nereids) add eliminate aggregation rule

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/EliminateAggregateConstant.java
@@ -0,0 +1,75 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.rules.rewrite;
+
+import org.apache.doris.nereids.rules.Rule;
+import org.apache.doris.nereids.rules.RuleType;
+import org.apache.doris.nereids.trees.expressions.Alias;
+import org.apache.doris.nereids.trees.expressions.NamedExpression;
+import org.apache.doris.nereids.trees.expressions.functions.agg.AggregateFunction;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Max;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Min;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Sum;
+import org.apache.doris.nereids.util.PlanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/** EliminateAggregateSum
+ * with group by:
+ * select max(1) from t1 group by c1; -> select 1 from t1;
+ * without group by:
+ * select max(1) from t1; -> select max(1) from (select 1 from t1 limit 1) tmp;
+ *
+ * extend:
+ * select max(1) + min(c1) from t1 group by c1;
+ * */
+public class EliminateAggregateConstant extends OneRewriteRuleFactory {
+    @Override
+    public Rule build() {
+        return logicalAggregate().then(agg -> {
+            // check whether we have aggregate(constant) in all aggregateFunctions
+            Set<AggregateFunction> aggFunctions = agg.getAggregateFunctions();
+            if (!aggFunctions.stream().allMatch(
+                    f -> (f instanceof Sum || f instanceof Min || f instanceof Max)
+                            && (f.arity() == 1 && f.child(0).isConstant()))) {
+                return null;
+            }
+
+            List<String> columnAliases = new ArrayList<>();
+            List<NamedExpression> newOutput = agg.getOutputExpressions().stream().map(ne -> {
+                if (ne instanceof Alias && ne.child(0) instanceof AggregateFunction) {
+                    AggregateFunction f = (AggregateFunction) ne.child(0);
+                    if (f instanceof Sum || f instanceof Min || f instanceof Max) {
+                        columnAliases.add(ne.getName());
+                        return new Alias(ne.getExprId(), f.child(0), ne.getName());
+                    } else {
+                        throw new IllegalStateException("Unexpected aggregate function: " + f);
+                    }
+                } else {
+                    return ne;
+                }
+            }).collect(Collectors.toList());
+            // make a new project to replace current aggregate
+            return PlanUtils.projectOrSelf(newOutput, agg.child());
+        }).toRule(RuleType.ELIMINATE_AGGREGATE_SUM);
+    }
+
+}
